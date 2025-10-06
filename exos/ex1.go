fa8bdb2083b16b6ae329532138574d7cb6ec1e25
Guillaume APCHAIN <guillaume.apchain@gmail.com>
2025-10-06 09:55:41 +0200

    chore: add ex1

---
+++ exos/ex1.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+)
+
+func moyenne(numbers ... float32) {
+	var somme float32 = 0
+	var nb_numbers float32 = float32(len(numbers))
+	log.Printf("Array size is %v", nb_numbers)
+	for _, n := range numbers {
+        somme += n
+    }
+    average := somme / nb_numbers
+    log.Printf("Calculated average is %v", average)
+
+}
+
+func main() {
+	moyenne(10,20,15.5,2.36,4.56,19.52)}
